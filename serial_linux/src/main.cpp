6a947a1c4262f39f90c4b21448c103e761788ec0
RuneScaping <runescapop@proton.me>
2022-01-07 08:50:27 +0000

    Update ignore files.

--- serial_linux/src/main.cpp
+++ serial_linux/src/main.cpp
@@ -1,4 +1,10 @@
 #include "serial.h"
 #include <iostream>
 
-using namespace s
+using namespace std;
+
+
+int main(int argc, char * argv[])
+{
+    string dev = "/dev/ttyUSB0";
+    Serial sel((char *
