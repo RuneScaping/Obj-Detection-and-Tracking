6679666ef30b8b76ccee799b357d641dc37d00f8
RuneScaping <runescapop@proton.me>
2022-01-08 07:42:39 +0000

    irda: sir_dev: Fix copy/paste typo

--- serial_linux/src/main.cpp
+++ serial_linux/src/main.cpp
@@ -7,4 +7,8 @@ using namespace std;
 int main(int argc, char * argv[])
 {
     string dev = "/dev/ttyUSB0";
-    Serial sel((char *
+    Serial sel((char *)dev.data());
+    sel.setPara();
+
+    string buff = "Hello!";
+    sel.writeData(buff.data(), buff.length());
