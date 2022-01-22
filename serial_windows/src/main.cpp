025b7f0fdb8f55a6579600573c4d09e3ad1abf0b
RuneScaping <runescapop@proton.me>
2022-01-22 14:30:27 +0000

    sso

--- serial_windows/src/main.cpp
+++ serial_windows/src/main.cpp
@@ -10,3 +10,10 @@ int main(int argc, char * argv[])
     string dev = "/dev/ttyUSB0";
     CSerial serial;
     serial.OpenSerialPort(_T("COM6"),115200,8,1);
+
+    string buff = "Hello!";
+    serial.SendData(buff.data(), buff.length());
+
+
+    return 0;
+}
