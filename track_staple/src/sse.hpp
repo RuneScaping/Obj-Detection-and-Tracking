ed986ed47efd6ae4e8cab66993a98f863ecbe794
RuneScaping <runescapop@proton.me>
2022-03-03 20:27:37 +0000

    Fix tooltips

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -2,4 +2,6 @@
 * Piotr's Computer Vision Matlab Toolbox      Version 3.23
 * Copyright 2014 Piotr Dollar.  [pdollar-at-gmail.com]
 * Licensed under the Simplified BSD License [see external/bsd.txt]
-************
+*******************************************************************************/
+#ifndef _SSE_HPP_
+#define _SSE
