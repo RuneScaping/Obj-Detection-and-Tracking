5d9309336749cb3d85ffb05f08386276eae06a88
RuneScaping <runescapop@proton.me>
2022-03-03 22:09:01 +0000

    version 0.10

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -4,4 +4,9 @@
 * Licensed under the Simplified BSD License [see external/bsd.txt]
 *******************************************************************************/
 #ifndef _SSE_HPP_
-#define _SSE
+#define _SSE_HPP_
+#include <emmintrin.h> // SSE2:<e*.h>, SSE3:<p*.h>, SSE4:<s*.h>
+
+namespace sse{
+
+#define R
