bdf1241315718ac9902686de1490c3367d3615ad
RuneScaping <runescapop@proton.me>
2022-03-03 19:02:15 +0000

    GameObject.Effect

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -1,3 +1,5 @@
 /*******************************************************************************
 * Piotr's Computer Vision Matlab Toolbox      Version 3.23
 * Copyright 2014 Piotr Dollar.  [pdollar-at-gmail.com]
+* Licensed under the Simplified BSD License [see external/bsd.txt]
+************
