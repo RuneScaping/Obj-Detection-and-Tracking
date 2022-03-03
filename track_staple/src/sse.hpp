4e48b8e0c20d6eb08c20150cbff11f879bc278d3
RuneScaping <runescapop@proton.me>
2022-03-03 17:34:43 +0000

    resolve conflicts with master

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -1,2 +1,3 @@
 /*******************************************************************************
-* Piotr's Computer Vision Matlab To
+* Piotr's Computer Vision Matlab Toolbox      Version 3.23
+* Copyright 2014 Piotr Dollar.  [pdollar-at-gmail.com]
