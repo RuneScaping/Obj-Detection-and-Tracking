603fa04df0de9384990b85f313f2fc79c926c1b1
RuneScaping <runescapop@proton.me>
2022-03-06 20:34:00 +0000

    add unit test

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -16,4 +16,5 @@ namespace sse{
 RETf SET( const float &x ) { return _mm_set1_ps(x); }
 RETf SET( float x, float y, float z, float w ) { return _mm_set_ps(x,y,z,w); }
 RETi SET( const int &x ) { return _mm_set1_epi32(x); }
-RETf LD( cons
+RETf LD( const float &x ) { return _mm_load_ps(&x); }
+RETf LDu( const float &x ) { return _mm_loadu_ps
