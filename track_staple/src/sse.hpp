f0af25847aacb33667bf2009a45906befc3cc11b
RuneScaping <runescapop@proton.me>
2022-03-07 18:02:50 +0000

    Prepare 1.1.6 release

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -19,4 +19,5 @@ RETi SET( const int &x ) { return _mm_set1_epi32(x); }
 RETf LD( const float &x ) { return _mm_load_ps(&x); }
 RETf LDu( const float &x ) { return _mm_loadu_ps(&x); }
 RETf STR( float &x, const __m128 y ) { _mm_store_ps(&x,y); return y; }
-RETf STR1( float
+RETf STR1( float &x, const __m128 y ) { _mm_store_ss(&x,y); return y; }
+RETf STRu( float &x, const __m128 y ) { _mm_s
