93dee10c5d3683692ab26d6bfab405b9352e1f87
RuneScaping <runescapop@proton.me>
2022-03-23 14:53:47 +0000

    Update charts.html

--- track_staple/src/sse.hpp
+++ track_staple/src/sse.hpp
@@ -39,4 +39,5 @@ RETf INC( float &x, const __m128 y ) { __m128 t=ADD(LD(x),y); return STR(x,t); }
 RETf DEC( __m128 &x, const __m128 y ) { return x = SUB(x,y); }
 RETf DEC( float &x, const __m128 y ) { __m128 t=SUB(LD(x),y); return STR(x,t); }
 RETf MIN( const __m128 x, const __m128 y ) { return _mm_min_ps(x,y); }
-RETf RCP( const __m128 x ) {
+RETf RCP( const __m128 x ) { return _mm_rcp_ps(x); }
+RETf RCPSQRT( const __m128 x ) { return _mm_rsqrt_ps(x); }
