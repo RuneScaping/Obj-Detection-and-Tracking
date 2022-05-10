981cb50828db7571bc4a4f145b79731dc35e3fb6
RuneScaping <runescapop@proton.me>
2022-05-10 07:30:37 +0000

    mvp

--- yolo_KCF/src/yolo_v2_class.hpp
+++ yolo_KCF/src/yolo_v2_class.hpp
@@ -225,3 +225,81 @@ public:
 
         if (prev_pts.rows == 0)
             prev_pts_flow_cpu = cv::Mat();
+        else
+            cv::transpose(prev_pts, prev_pts_flow_cpu);
+
+        if (prev_pts_flow_gpu.cols < prev_pts_flow_cpu.cols) {
+            prev_pts_flow_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), prev_pts_flow_cpu.type());
+            cur_pts_flow_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), prev_pts_flow_cpu.type());
+
+            status_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), CV_8UC1);
+            err_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), CV_32FC1);
+        }
+
+        prev_pts_flow_gpu.upload(cv::Mat(prev_pts_flow_cpu), stream);
+    }
+
+
+    void update_tracking_flow(cv::Mat src_mat, std::vector<bbox_t> _cur_bbox_vec)
+    {
+        int const old_gpu_id = cv::cuda::getDevice();
+        if (old_gpu_id != gpu_id)
+            cv::cuda::setDevice(gpu_id);
+
+        if (src_mat.channels() == 3) {
+            if (src_mat_gpu.cols == 0) {
+                src_mat_gpu = cv::cuda::GpuMat(src_mat.size(), src_mat.type());
+                src_grey_gpu = cv::cuda::GpuMat(src_mat.size(), CV_8UC1);
+            }
+
+            update_cur_bbox_vec(_cur_bbox_vec);
+
+            //src_grey_gpu.upload(src_mat, stream);    // use BGR
+            src_mat_gpu.upload(src_mat, stream);
+            cv::cuda::cvtColor(src_mat_gpu, src_grey_gpu, CV_BGR2GRAY, 1, stream);
+        }
+        if (old_gpu_id != gpu_id)
+            cv::cuda::setDevice(old_gpu_id);
+    }
+
+
+    std::vector<bbox_t> tracking_flow(cv::Mat dst_mat, bool check_error = true)
+    {
+        if (sync_PyrLKOpticalFlow_gpu.empty()) {
+            std::cout << "sync_PyrLKOpticalFlow_gpu isn't initialized \n";
+            return cur_bbox_vec;
+        }
+
+        int const old_gpu_id = cv::cuda::getDevice();
+        if(old_gpu_id != gpu_id)
+            cv::cuda::setDevice(gpu_id);
+
+        if (dst_mat_gpu.cols == 0) {
+            dst_mat_gpu = cv::cuda::GpuMat(dst_mat.size(), dst_mat.type());
+            dst_grey_gpu = cv::cuda::GpuMat(dst_mat.size(), CV_8UC1);
+        }
+
+        //dst_grey_gpu.upload(dst_mat, stream);    // use BGR
+        dst_mat_gpu.upload(dst_mat, stream);
+        cv::cuda::cvtColor(dst_mat_gpu, dst_grey_gpu, CV_BGR2GRAY, 1, stream);
+
+        if (src_grey_gpu.rows != dst_grey_gpu.rows || src_grey_gpu.cols != dst_grey_gpu.cols) {
+            stream.waitForCompletion();
+            src_grey_gpu = dst_grey_gpu.clone();
+            cv::cuda::setDevice(old_gpu_id);
+            return cur_bbox_vec;
+        }
+
+        ////sync_PyrLKOpticalFlow_gpu.sparse(src_grey_gpu, dst_grey_gpu, prev_pts_flow_gpu, cur_pts_flow_gpu, status_gpu, &err_gpu);    // OpenCV 2.4.x
+        sync_PyrLKOpticalFlow_gpu->calc(src_grey_gpu, dst_grey_gpu, prev_pts_flow_gpu, cur_pts_flow_gpu, status_gpu, err_gpu, stream);    // OpenCV 3.x
+
+        cv::Mat cur_pts_flow_cpu;
+        cur_pts_flow_gpu.download(cur_pts_flow_cpu, stream);
+
+        dst_grey_gpu.copyTo(src_grey_gpu, stream);
+
+        cv::Mat err_cpu, status_cpu;
+        err_gpu.download(err_cpu, stream);
+        status_gpu.download(status_cpu, stream);
+
+        stream.waitForCompletion();
