536931e72dd365b955ebf65dbf6062227088c858
RuneScaping <runescapop@proton.me>
2022-06-26 13:47:10 +0000

    new buildframeworkd distributed

--- yolo_TM/src/yolo_v2_class.hpp
+++ yolo_TM/src/yolo_v2_class.hpp
@@ -152,3 +152,109 @@ private:
         out.c = c;
         return out;
     }
+
+    static image_t make_image_custom(int w, int h, int c)
+    {
+        image_t out = make_empty_image(w, h, c);
+        out.data = (float *)calloc(h*w*c, sizeof(float));
+        return out;
+    }
+
+#endif    // OPENCV
+
+};
+
+
+
+#if defined(TRACK_OPTFLOW) && defined(OPENCV) && defined(GPU)
+
+#include <opencv2/cudaoptflow.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/core/cuda.hpp>
+
+class Tracker_optflow {
+public:
+    const int gpu_count;
+    const int gpu_id;
+    const int flow_error;
+
+
+    Tracker_optflow(int _gpu_id = 0, int win_size = 9, int max_level = 3, int iterations = 8000, int _flow_error = -1) :
+        gpu_count(cv::cuda::getCudaEnabledDeviceCount()), gpu_id(std::min(_gpu_id, gpu_count-1)),
+        flow_error((_flow_error > 0)? _flow_error:(win_size*4))
+    {
+        int const old_gpu_id = cv::cuda::getDevice();
+        cv::cuda::setDevice(gpu_id);
+
+        stream = cv::cuda::Stream();
+
+        sync_PyrLKOpticalFlow_gpu = cv::cuda::SparsePyrLKOpticalFlow::create();
+        sync_PyrLKOpticalFlow_gpu->setWinSize(cv::Size(win_size, win_size));    // 9, 15, 21, 31
+        sync_PyrLKOpticalFlow_gpu->setMaxLevel(max_level);        // +- 3 pt
+        sync_PyrLKOpticalFlow_gpu->setNumIters(iterations);    // 2000, def: 30
+
+        cv::cuda::setDevice(old_gpu_id);
+    }
+
+    // just to avoid extra allocations
+    cv::cuda::GpuMat src_mat_gpu;
+    cv::cuda::GpuMat dst_mat_gpu, dst_grey_gpu;
+    cv::cuda::GpuMat prev_pts_flow_gpu, cur_pts_flow_gpu;
+    cv::cuda::GpuMat status_gpu, err_gpu;
+
+    cv::cuda::GpuMat src_grey_gpu;    // used in both functions
+    cv::Ptr<cv::cuda::SparsePyrLKOpticalFlow> sync_PyrLKOpticalFlow_gpu;
+    cv::cuda::Stream stream;
+
+    std::vector<bbox_t> cur_bbox_vec;
+    std::vector<bool> good_bbox_vec_flags;
+    cv::Mat prev_pts_flow_cpu;
+
+    void update_cur_bbox_vec(std::vector<bbox_t> _cur_bbox_vec)
+    {
+        cur_bbox_vec = _cur_bbox_vec;
+        good_bbox_vec_flags = std::vector<bool>(cur_bbox_vec.size(), true);
+        cv::Mat prev_pts, cur_pts_flow_cpu;
+
+        for (auto &i : cur_bbox_vec) {
+            float x_center = (i.x + i.w / 2.0F);
+            float y_center = (i.y + i.h / 2.0F);
+            prev_pts.push_back(cv::Point2f(x_center, y_center));
+        }
+
+        if (prev_pts.rows == 0)
+            prev_pts_flow_cpu = cv::Mat();
+        else
+            cv::transpose(prev_pts, prev_pts_flow_cpu);
+
+        if (prev_pts_flow_gpu.cols < prev_pts_flow_cpu.cols) {
+            prev_pts_flow_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), prev_pts_flow_cpu.type());
+            cur_pts_flow_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), prev_pts_flow_cpu.type());
+
+            status_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), CV_8UC1);
+            err_gpu = cv::cuda::GpuMat(prev_pts_flow_cpu.size(), CV_32FC1);
+        }
+
+        prev_pts_flow_gpu.upload(cv::Mat(prev_pts_flow_cpu), stream);
+    }
+
+
+    void update_tracking_flow(cv::Mat src_mat, std::vector<bbox_t> _cur_bbox_vec)
+    {
+        int const old_gpu_id = cv::cuda::getDevice();
+        if (old_gpu_id != gpu_id)
+            cv::cuda::setDevice(gpu_id);
+
+        if (src_mat.channels() == 3) {
+            if (src_mat_gpu.cols == 0) {
+                src_mat_gpu = cv::cuda::GpuMat(src_mat.size(), src_mat.type());
+                src_grey_gpu = cv::cuda::GpuMat(src_mat.size(), CV_8UC1);
+            }
+
+            update_cur_bbox_vec(_cur_bbox_vec);
+
+            //src_grey_gpu.upload(src_mat, stream);    // use BGR
+            src_mat_gpu.upload(src_mat, stream);
+            cv::cuda::cvtColor(src_mat_gpu, src_grey_gpu, CV_BGR2GRAY, 1, stream);
+        }
